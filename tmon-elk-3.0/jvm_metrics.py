8475f289c4cc179a7995a8926c795b7c4640f1a4
cesar <cmesquita00@hotmail.com>
2015-11-10 13:48:30 -0200

    atualizacao

--- tmon-elk-3.0/jvm_metrics.py
+++ tmon-elk-3.0/jvm_metrics.py
@@ -21,10 +21,35 @@ def getJVMmetrics():
               # pass
                 print "Ignoring exception " + e.getMessage()
         
- 
 def getOpenSockets():
-	
+	while 1:
+        serverNames = getRunningServerNames()
+        domainRuntime()
+        for name in serverNames:
+            print 'Now checking '+name.getName()
+            try:
+              cd("/ServerRuntimes/"+name.getName())
+              getOpenSocketsCurrentCount = cmo.getOpenSocketsCurrentCount()
+			  print getOpenSocketsCurrentCount
+            except WLSTException,e:
+              # this typically means the server is not active, just ignore
+              # pass
+                print "Ignoring exception " + e.getMessage()
+
 def getHTTPSessions():
+	while 1:
+        serverNames = getRunningServerNames()
+        domainRuntime()
+        for name in serverNames:
+            print 'Now checking '+name.getName()
+            try:
+              cd("/ServerRuntimes/"+name.getName())
+              OpenSessionCurrentCount = cmo.getOpenSessionsCurrentCount()
+			  print OpenSessionCurrentCount
+            except WLSTException,e:
+              # this typically means the server is not active, just ignore
+              # pass
+                print "Ignoring exception " + e.getMessage()
 	
 def getGCElapsedTime():
 	
