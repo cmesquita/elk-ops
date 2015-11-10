6d447f9829bc430494ab9b4194068419d7b51048
cesar <cmesquita00@hotmail.com>
2015-11-10 13:12:47 -0200

    it gets jvm metrics

---
+++ jvm_metrics.py
@@ -0,0 +1,39 @@
+#jvm_metrics.py
+user = "weblogic"
+password = "passw0rd"
+connect_string = "t3://172.16.3.125:7001"
+
+# connecting weblogic	 
+connect(user,password,connect_string)
+
+def getJVMmetrics():
+	while 1:
+        serverNames = getRunningServerNames()
+        domainRuntime()
+        for name in serverNames:
+            print 'Now checking '+name.getName()
+            try:
+              cd("/ServerRuntimes/"+name.getName()+"/JVMRuntime/"+name.getName())
+              heapSize = cmo.getHeapSizeCurrent()
+              print heapSize
+            except WLSTException,e:
+              # this typically means the server is not active, just ignore
+              # pass
+                print "Ignoring exception " + e.getMessage()
+        
+ 
+def getOpenSockets():
+	
+def getHTTPSessions():
+	
+def getGCElapsedTime():
+	
+def getTimeStamp():
+	
+def getRunningServerNames():
+	# only returns the currently running servers in the domain
+	return domainRuntimeService.getServerRuntimes()
+ 
+if __name__== "main":
+	getJVMmetrics()
+	disconnect()
