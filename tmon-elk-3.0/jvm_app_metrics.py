96da97afdaac7e48a4ec1da9a790910ae1faf19d
oracle <oracle@weblogic.alcatraz.local>
2015-11-17 07:45:50 -0800

    md5 function support

--- tmon-elk-3.0/jvm_app_metrics.py
+++ tmon-elk-3.0/jvm_app_metrics.py
@@ -1,6 +1,7 @@
 #jvm_metrics.py
 import time as pytime
-import hashlib
+import md5
+import ConfigParser, os
 
 user = "weblogic"
 password = "manager1"
@@ -9,10 +10,13 @@ connect_string = "t3://192.168.47.205:7001"
 # connecting weblogic	 
 connect(user,password,connect_string)
 
-def stackHash():
-	print(hashlib.algorithms_available)
-	print(hashlib.algorithms_guaranteed)
-
+def getStackHash():
+	m=md5.new()
+	m.update("test test")
+	output = m.digest()
+	hexoutput = m.hexdigest()
+	print("digest= " + str(output))
+	print("digest= " + str(hexoutput))	
 
 def getTest():
 	domainConfig()
@@ -78,5 +82,6 @@ def getAppStatus():
 	return cmo.getApplicationIds()
 
 if __name__== "main":
-	getHTTPSessions()
+	#getHTTPSessions()
 	#getTest()
+	getStackHash()
